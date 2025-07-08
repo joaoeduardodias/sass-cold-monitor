54ab692326bcbdf35e61856ea5883e4a5578355d
João Dias <joaoeduardodias123@gmail.com>
2025-07-08 17:20:33 -0300

    feat: add docs and auto join organization by domain email

---
+++ apps/api/src/http/routes/create-user.ts
@@ -0,0 +1,52 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from 'zod/v4';
+export async function createUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(1, "Name is required"),
+          email: z.email("Invalid e-mail format"),
+          password: z.string().min(6, "Password must be at least 6 characters long"),
+        })
+      }
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email }
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({ error: "User with same e-mail already exists." });
+      }
+
+      const [, domain] = email.split('@');
+
+      const autoJoinOrganization = await prisma.organization.findUnique({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        }
+      })
+
+      const passwordHash = await hash(password, 6);
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+          member_on: autoJoinOrganization ? {
+            create: {
+              organizationId: autoJoinOrganization.id
+            }
+          } : undefined
+        }
+      })
+      return reply.status(201).send()
+    })
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,22 +1,38 @@
 
 import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 import { fastify } from 'fastify';
 import {
-  //  jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
 } from 'fastify-type-provider-zod';
-import { createAccount } from './routes/create-account';
+import { createUser } from './routes/create-user';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 app.register(fastifyCors)
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Cold Monitor',
+      description: 'App monitoring cold',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
 
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+});
 
-app.register(createAccount)
+app.register(createUser)
 
 app.listen({ port: 3001 }).then(() => {
   console.log("Server is running on http://localhost:3001")
