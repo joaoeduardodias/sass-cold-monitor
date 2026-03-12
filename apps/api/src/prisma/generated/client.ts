e72f369e339c3f3c9c96fd69ebab38431bb532c1
João Dias <joaoeduardodias123@gmail.com>
2026-03-12 08:52:25 -0300

    fix: eror url

--- apps/api/src/prisma/generated/client.ts
+++ apps/api/src/prisma/generated/client.ts
@@ -10,18 +10,17 @@
  * 🟢 You can import this file directly.
  */
 
-import * as process from 'node:process'
 import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
-globalThis['__dirname'] = path.dirname(fileURLToPath(import.meta.url))
+globalThis['__dirname'] = path.dirname(fileURLToPath(new URL(import.meta.url)))
 
 import * as runtime from "@prisma/client/runtime/client"
-import * as $Enums from "./enums.js"
 import * as $Class from "./internal/class.js"
 import * as Prisma from "./internal/prismaNamespace.js"
 
-export * as $Enums from './enums.js'
 export * from "./enums.js"
+export * as $Enums from './enums.js'
+export { Prisma }
 /**
  * ## Prisma Client
  * 
@@ -37,7 +36,6 @@ export * from "./enums.js"
  */
 export const PrismaClient = $Class.getPrismaClientClass()
 export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts extends Prisma.PrismaClientOptions["omit"] = Prisma.PrismaClientOptions["omit"], ExtArgs extends runtime.Types.Extensions.InternalArgs = runtime.Types.Extensions.DefaultArgs> = $Class.PrismaClient<LogOpts, OmitOpts, ExtArgs>
-export { Prisma }
 
 /**
  * Model User
