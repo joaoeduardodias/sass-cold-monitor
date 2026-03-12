16e4fe495a712be75f65c244008478dc9041bcf2
João Dias <joaoeduardodias123@gmail.com>
2026-03-12 07:59:00 -0300

    fix: remove shared Next plublic

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,5 +1,5 @@
 import { createEnv } from '@t3-oss/env-nextjs'
-import { z } from 'zod/v4'
+import { z } from 'zod'
 
 export const env = createEnv({
   server: {
@@ -17,9 +17,9 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.url(),
   },
-  shared: {
-    NEXT_PUBLIC_API_URL: z.url(),
-  },
+  // shared: {
+  //   NEXT_PUBLIC_API_URL: z.url(),
+  // },
   runtimeEnv: {
     PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
